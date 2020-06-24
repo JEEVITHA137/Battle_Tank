7a0f517929ec1a21f637ef4b5aa5c829d6b4bbd6
jeevitha <jeevithasakthi137@gmail.com>
2020-06-24 16:51:09 +0530

    Setting Up Movement

--- Battle_Tank/Source/Battle_Tank/Private/TankMovementComponent.cpp
+++ Battle_Tank/Source/Battle_Tank/Private/TankMovementComponent.cpp
@@ -2,8 +2,30 @@
 
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialise(UTankTrack *LeftTrackToSet, UTankTrack *RightTrackToSet)
+{
+    if (!LeftTrackToSet || !RightTrackToSet)
+    {
+        return;
+    }
+    LeftTrack = LeftTrackToSet;
+    RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
+    if (!LeftTrack || !RightTrack )
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
     UE_LOG(LogTemp, Warning, TEXT("IntendMoveForward: %f"), Throw);
+
+    if (!LeftTrack || !RightTrack )
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(Throw);
 }
