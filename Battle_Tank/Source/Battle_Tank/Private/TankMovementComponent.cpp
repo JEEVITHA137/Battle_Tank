f40ba43289afafd793833653cbb3bb8a08ea2ffd
jeevitha <jeevithasakthi137@gmail.com>
2020-07-07 19:47:25 +0530

    Finalising Your Class Code

--- Battle_Tank/Source/Battle_Tank/Private/TankMovementComponent.cpp
+++ Battle_Tank/Source/Battle_Tank/Private/TankMovementComponent.cpp
@@ -1,5 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Copyright @2020
 
 #include "TankMovementComponent.h"
 #include "TankTrack.h"
--- Battle_Tank/Source/Battle_Tank/Public/TankMovementComponent.h
+++ Battle_Tank/Source/Battle_Tank/Public/TankMovementComponent.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright @2020
 
 #pragma once
 
@@ -17,18 +17,17 @@ class BATTLE_TANK_API UTankMovementComponent : public UNavMovementComponent
 	GENERATED_BODY()
 	
 public:
-    UFUNCTION(BluePrintCallable,Category = Input)
+    UFUNCTION(BluePrintCallable,Category = "Input")
     void IntendMoveForward(float Throw);
 
-	UFUNCTION(BluePrintCallable,Category = Input)
+	UFUNCTION(BluePrintCallable,Category = "Input")
     void IntendTurnRight(float Throw);
 
-	UFUNCTION(BlueprintCallable, Category = Setup)
+	UFUNCTION(BlueprintCallable, Category = "Setup")
 	void Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
 
-	virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
-
 private:
+	virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
 	UTankTrack* LeftTrack = nullptr;
 	UTankTrack* RightTrack = nullptr;
 };
