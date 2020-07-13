2332b9db2dedf123b0cc0ac36d183c2a0c5c4923
jeevitha <jeevithasakthi137@gmail.com>
2020-07-13 20:04:48 +0530

    OnComponentHit Event

--- Battle_Tank/Source/Battle_Tank/Public/TankTrack.h
+++ Battle_Tank/Source/Battle_Tank/Public/TankTrack.h
@@ -26,5 +26,10 @@ class BATTLE_TANK_API UTankTrack : public UStaticMeshComponent
  private:
 	 UTankTrack();
 
-	 virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+	 virtual void BeginPlay() override;
+
+	 virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	 UFUNCTION()
+	 void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult &Hit);
 };
